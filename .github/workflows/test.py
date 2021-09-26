576d6add7e2b80555914e466bc9bd34d4ac1656d
huangluo0323 <54200083+huangluo0323@users.noreply.github.com>
2021-09-26 15:18:51 +0800

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,2 +1,19 @@
-data = "成功"
-print(data)
+name: Huangluo Test
+
+on:
+  push:
+    branches:
+      - master
+      - slave
+jobs:
+  test_qa:
+    if: ${{ github.ref == 'refs/heads/slave' }}
+    runs-on: ubuntu-latest
+    # Steps represent a sequence of tasks that will be executed as part of the job
+    steps:
+      - run: echo "Test qa"
+
+  deployment_test_prd:
+    if: ${{ github.ref == 'refs/heads/master' }}
+    runs-on: ubuntu-latest
+      - run: echo "Test prd"
